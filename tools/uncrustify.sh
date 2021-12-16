3fca742a0ab74d69c76c18f1c7c89f28be98ab16
Andrei Pavel <andrei@isc.org>
2021-12-16 18:01:59 +0200

    [#2200] fix formatting tools on premium or other repos

--- tools/uncrustify.sh
+++ tools/uncrustify.sh
@@ -38,7 +38,7 @@ while test ${#} -gt 0; do
     '-d'|'--debug') set -vx ;;
 
     # [-h|--help]              print usage (this text).
-    '-h'|'--help') print_usage ;;
+    '-h'|'--help') print_usage; exit 0 ;;
 
     # Allow extra arguments, they should be directories or files to be formatted.
     *) break ;;
@@ -85,6 +85,13 @@ while test ${#} -gt 0; do
     # We specifically want word splitting for the parameters.
     uncrustify -c "${script_path}/../.uncrustify.cfg" --replace $(printf '%s' "${parameters}") "${file}"
   elif test -d "${file}"; then
+    # Keep CWD for later use.
+    pwd="${PWD}"
+
+    # First checkout target directory so that the following "git ls-files" call
+    # acts on the proper git repository.
+    cd "${file}"
+
     # Get list of files to format.
     cd "$(git rev-parse --show-toplevel)"
     files=$(git ls-files | xargs -n1 printf "${PWD}/%s\\n" | grep -F "${file}" \
@@ -95,8 +102,13 @@ while test ${#} -gt 0; do
       files=$(printf '%s\n' "${files}" | grep -Fv "${file}" | sed '/^$/d')
     done
 
+    # Restore directory to what it initially was because ${files} contains
+    # relative paths.
+    cd "${pwd}"
+
     # For all files...
     for i in ${files}; do
+      # Run recursively to format file or iterate further through directories.
       "${0}" "${i}"
     done
   fi
