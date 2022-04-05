ecb26f15fd4f0be2b89b2ce5f94ce63d30fa9831
Francis Dupont <fdupont@isc.org>
2022-04-05 14:55:19 +0200

    [#1263] Checkpoint: finished callout tests, doing doc

--- src/hooks/agent/rbac/tests/rbac_callout_unittests.cc
+++ src/hooks/agent/rbac/tests/rbac_callout_unittests.cc
@@ -13,11 +13,13 @@
 #include <hooks/hooks.h>
 #include <http/post_request_json.h>
 #include <http/response_json.h>
-#include <testutils/gtest_utils.h>
+#include <testutils/log_utils.h>
+#include <cstdlib>
 
 using namespace isc;
 using namespace isc::config;
 using namespace isc::data;
+using namespace isc::dhcp::test;
 using namespace isc::hooks;
 using namespace isc::http;
 using namespace isc::rbac;
@@ -50,11 +52,51 @@ const char* CONFIGS[] = {
     "            \"other-commands\": false\n"
     "        }\n"
     "    ]\n"
-    "}\n"
+    "}\n",
+
+    // configuration 1: same with other-commands to true.
+    "{\n"
+    "    \"assign-role-method\": \"remote-address\",\n"
+    "    \"roles\":\n"
+    "    [\n"
+    "        {\n"
+    "            \"name\": \"127.0.0.1\",\n"
+    "            \"accept-commands\":\n"
+    "            {\n"
+    "                \"commands\":\n"
+    "                [\n"
+    "                    \"list-commands\"\n"
+    "                ]\n"
+    "            },\n"
+    "            \"other-commands\": true\n"
+    "        }\n"
+    "    ]\n"
+    "}\n",
+
+    // configuration 2: now with require-tls.
+    "{\n"
+    "    \"assign-role-method\": \"remote-address\",\n"
+    "    \"require-tls\": true,\n"
+    "    \"roles\":\n"
+    "    [\n"
+    "        {\n"
+    "            \"name\": \"127.0.0.1\",\n"
+    "            \"accept-commands\":\n"
+    "            {\n"
+    "                \"commands\":\n"
+    "                [\n"
+    "                    \"list-commands\"\n"
+    "                ]\n"
+    "            },\n"
+    "            \"other-commands\": false\n"
+    "        }\n"
+    "    ]\n"
+    "}\n",
+
 };
 
 /// @brief Test fixture for check RBAC callouts.
-class CalloutTest : public ::testing::Test {
+class CalloutTest : public LogContentTest {
 public:
 
     /// @brief Constructor.
@@ -145,6 +187,10 @@ TEST_F(CalloutTest, accept) {
     HttpResponseJsonPtr response;
     authCalloutCall(request, response);
     EXPECT_FALSE(response);
+    string expected = "RBAC_TRACE_AUTH_ACCEPT ";
+    expected += "Role configuration '127.0.0.1' for role '127.0.0.1' ";
+    expected += "has accepted command 'list-commands'.";
+    EXPECT_EQ(1, countFile(expected));
 }
 
 /// @brief This test verifies that reject works as expected.
@@ -155,6 +201,64 @@ TEST_F(CalloutTest, reject) {
     HttpResponseJsonPtr response;
     authCalloutCall(request, response);
     EXPECT_TRUE(response);
+    string expected = "RBAC_TRACE_AUTH_REJECT ";
+    expected += "Role configuration '127.0.0.1' for role '127.0.0.1' ";
+    expected += "has rejected command 'status-get'.";
+    EXPECT_EQ(1, countFile(expected));
+}
+
+/// @brief This test verifies that other commands works as expected.
+TEST_F(CalloutTest, otherCommands) {
+    configure(CONFIGS[1]);
+    RoleConfigPtr rc = roleConfigTable["127.0.0.1"];
+    HttpRequestPtr request = createHttpRequest("status-get", "dhcp4");
+    request->setRemote("127.0.0.1");
+    HttpResponseJsonPtr response;
+    authCalloutCall(request, response);
+    EXPECT_FALSE(response);
+    string expected = "RBAC_TRACE_AUTH_ACCEPT ";
+    expected += "Role configuration '127.0.0.1' for role '127.0.0.1' ";
+    expected += "has accepted command 'status-get'.";
+    EXPECT_EQ(1, countFile(expected));
+}
+
+/// @brief This test verifies that default role works as expected.
+TEST_F(CalloutTest, defaultRole) {
+    configure(CONFIGS[0]);
+    HttpRequestPtr request = createHttpRequest("list-commands", "dhcp6");
+    // no remote.
+    HttpResponseJsonPtr response;
+    authCalloutCall(request, response);
+    EXPECT_TRUE(response);
+    string expected = "RBAC_TRACE_AUTH_REJECT ";
+    expected += "Role configuration 'default/reject' for role '' ";
+    expected += "has rejected command 'list-commands'.";
+    EXPECT_EQ(1, countFile(expected));
+}
+
+/// @brief This test verifies that unknown role works as expected.
+TEST_F(CalloutTest, unknowntRole) {
+    configure(CONFIGS[0]);
+    HttpRequestPtr request = createHttpRequest("list-commands", "dhcp6");
+    request->setRemote("::1");
+    HttpResponseJsonPtr response;
+    authCalloutCall(request, response);
+    EXPECT_TRUE(response);
+    string expected = "RBAC_TRACE_AUTH_REJECT ";
+    expected += "Role configuration 'unknown/reject' for role '::1' ";
+    expected += "has rejected command 'list-commands'.";
+    EXPECT_EQ(1, countFile(expected));
+}
+
+/// @brief This test verifies that require TLS works as expected.
+TEST_F(CalloutTest, requireTls) {
+    configure(CONFIGS[2]);
+    HttpRequestPtr request = createHttpRequest("list-commands", "dhcp6");
+    // Require TLS default value is false.
+    HttpResponseJsonPtr response;
+    authCalloutCall(request, response);
+    EXPECT_TRUE(response);
+    EXPECT_EQ(1, countFile("RBAC_TRACE_AUTH_NO_TLS_REJECT"));
 }
 
 } // end of anonymous namespace
